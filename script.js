b427960164fdd98fe32150dcbfd6c9abf7e55d57
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-09-12 16:09:57 +0200

    proseguo esercizio 3

--- script.js
+++ script.js
@@ -1,36 +1,71 @@
 // 1 Crea una funzione che controlli due numeri Interi. Ritorna True se uno dei due è 50 o la loro somma è 50.
 
-function checkInteger(numberA, numberB) {
-    if (!Number.isInteger(numberA) || !Number.isInteger(numberB))
-        console.log('Non e un numero intero')
-    else sum(numberA,numberB)
-}
+// function round(numberA,numberB){
+//     let roundA= Math.round(numberA)
+//     let roundB=Math.round(numberB)
+//     let operation= roundA+roundB
+//     if (roundA === 50 || roundB === 50) {
+//         result = true
+//     }
+//     else if (roundA + roundB === 50) {
+//         result = true
+//     }
+//     else result= false
+//     console.log(operation)
+//     console.log(result)
+
+// }
+// round(50,5)
+
+// function checkInteger(numberA, numberB) {
+//     if (!Number.isInteger(numberA) || !Number.isInteger(numberB))
+//         console.log('Non e un numero intero')
+//     else sum(numberA,numberB)
+// }
 
 
-function sum(numberA, numberB) {
-    let operation= numberA+numberB
-    if (numberA === 50 || numberB === 50) {
-        result = true
-    }
-    else if (numberA + numberB === 50) {
-        result = true
-    }
-    else result= false
-    console.log(operation)
-    console.log(result)
-}
-
-// checkInteger(25.5, 25)
+// function sum(numberA, numberB) {
+//     let operation= numberA+numberB
+//     if (numberA === 50 || numberB === 50) {
+//         result = true
+//     }
+//     else if (numberA + numberB === 50) {
+//         result = true
+//     }
+//     else result= false
+//     console.log(operation)
+//     console.log(result)
+// }
 
 // Esercizio 2
 // Crea una funzione che rimuova il carattere ad una specifica posizione da una stringa. 
 // Passa la stringa e la posisione come parametri e ritorna la stringa modificata
 
-function changeStringa
+// function sliceString(string, start,end){
+//     let newString = string.slice(start,end)
+//     console.log(newString)
+// }
+// sliceString('banana',0,1)
+
+// esercizio 3
+// Crea una funzione che controlli se due numeri siano compresi tra 40 e 60 o tra 70 e 100. ritorna true sew rispecchiano condizioni oppure false
+
+function checkNumber(a, b) {
+   
+    if (!a >= 40 && !a <= 70 || !a >= 70 && !a <= 100) {
+        a = false
+    } else if (!b >= 40 && !b <= 70 || !b >= 70 && !b <= 100) {
+        b = false
+    }
 
 
 
+    console.log(a)
+    console.log(b)
+}
 
+
+checkNumber(2, 70)
 
 // Esercizi extra
 // NOTA: tutti gli esercizi devono essere svolti usando le funzioni
